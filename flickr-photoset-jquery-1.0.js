716c0363fe32fddab90fa55769d4dbd8191f243b
Eric <zpalffy@gmail.com>
2015-03-18 17:51:02 -0500

    added example and a few small changes.

--- flickr-photoset-jquery-1.0.js
+++ flickr-photoset-jquery-1.0.js
@@ -1,3 +1,10 @@
+/*****************************************************************************************
+ * flickr-photoset-jquery
+ * Developed by Eric Olson, https://github.com/zpalffy
+ * Useage is explained here: https://github.com/zpalffy/flickr-photoset-jquery
+ * License: The MIT License
+******************************************************************************************/
+
 (function ($) {
     merge = function(template, model) {
         for (attr in model) {
@@ -10,10 +17,11 @@
     $.fn.flickrPhotoset = function(apiKey, id, options) {
         var opts = $.extend({
             before: '<h3><a href="{{url}}">{{title}}</a></h3>',
-            template: '<a href="{{link}}"><img src="{{img}}_q.jpg" alt="{{title}}"/></a>'
+            template: '<a href="{{link}}" title="{{title}}"><img src="{{img}}_q.jpg" alt="{{title}}"/></a>'
         }, options);
         
         return this.each(function() {
+            // api key can be found/created here: https://www.flickr.com/services/api/keys/
             if (id && apiKey) {
                 var ele = $(this);
 
@@ -32,6 +40,7 @@
                     },
 
                     success: function(set) {
+                        if(!set.photoset) return;
                         var photos = $.extend(set.photoset, {
                             url: 'https://www.flickr.com/photos/' + set.photoset.owner + '/sets/' + set.photoset.id
                         });
@@ -46,14 +55,13 @@
 
                         if (opts.template) {
                             $(photos.photo).each(function() {
-                                var attrs = {
+                                ele.append(merge(opts.template, {
+                                    // see https://www.flickr.com/services/api/misc.urls.html for info on url formats.
                                     photosetId: photos.id,
                                     link: 'https://www.flickr.com/photos/' + photos.owner + '/' + this.id,
                                     img: 'https://farm' + this.farm + '.staticflickr.com/' + this.server + '/' + this.id + '_' + this.secret,
                                     title: this.title
-                                };
-
-                                ele.append(merge(opts.template, attrs));
+                                }));
                             });
                         }
 
